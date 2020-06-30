dd464f16b366340aeae312212ab390a0d6baad10
USUARIO-HP <USUARIO-HP@DESKTOP-4Q42L19>
2020-06-30 01:20:14 -0500

    Cambios :V

---
+++ SlnDelivery/PryDelivery/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PryDelivery.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Enter(string user, string password)
+        {
+
+
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocuriio un error:(" + ex.Message);
+
+            }
+
+        }
+    }
+}
